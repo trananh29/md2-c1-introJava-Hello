c574814d6ac6c185194125fe3e70421325851df0
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-03-23 15:09:21 +0700

    hello

---
+++ src/main/java/greaTing.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class greaTing {
+    public static void main(String[] args) {
+        String name;
+        System.out.println("Enter your name");
+        Scanner name1= new Scanner(System.in);
+        name=name1.nextLine();
+        System.out.printf("Hello: %s",name);
+    }
+}
